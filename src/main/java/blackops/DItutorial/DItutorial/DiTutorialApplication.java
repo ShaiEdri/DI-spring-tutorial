aa0be1b181f8206fb447ec2372fff828cd3300f6
shaiedri <shaiedri850@gmail.com>
2023-03-28 17:43:57 +0300

    Initial

---
+++ src/main/java/blackops/DItutorial/DItutorial/DiTutorialApplication.java
@@ -0,0 +1,13 @@
+package blackops.DItutorial.DItutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/blackops/DItutorial/DItutorial/DiTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package blackops.DItutorial.DItutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
